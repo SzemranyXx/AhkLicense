405bc178d51e755d5fbdd9587e538026fa386876
Szemrany__ <104011057+SzemranyXx@users.noreply.github.com>
2022-04-19 16:59:52 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    botConfig: {
+        token: "ODAzNjUxNjgxNjQ4Mzc3ODY2.YBA49A.xNQi3A3-U1I2b4bMAd7ghY7R_XI",
+        prefix: ".",
+        ownerid: "778529039417606214",
+        kolor_embeda: "GREEN",
+        kolor_embeda_bledu: "RED",
+        uprlicencja: "940719512490246255",
+    },
+}
---
+++ index.js
@@ -0,0 +1,111 @@
+require(`dotenv`).config()
+require(`discord-reply`)
+global.Discord = require(`discord.js`)
+global.client = new Discord.Client()
+require(`events`).EventEmitter.defaultMaxListeners = Infinity
+global.axios = require(`axios`)
+global.fs = require(`fs`)
+global.path = require(`path`)
+global.jsonWriter = require(`fs-json-writer`)
+global.ytdl = require(`ytdl-core`)
+global.chalk = require(`chalk`)
+global.math = require(`mathjs`)
+global.emoji = require(`discord-emoji-convert`)
+global.db = require(`quick.db`)
+global.emotki = require(`./emotki.json`)
+global.uprawnieniaa = require(`./uprawnienia.json`)
+client.config = require('./config.js')
+
+global.gprefix = client.config.botConfig.prefix
+global.kolor_embeda = client.config.botConfig.kolor_embeda
+global.kolor_embeda_bledu = client.config.botConfig.kolor_embeda_bledu
+global.ownerid = client.config.botConfig.ownerid;
+global.token = client.config.botConfig.token;
+
+global.uprlicencja = client.config.botConfig.uprlicencja;
+
+
+
+global.fetch = require(`node-fetch`)
+
+const mysql = require(`mysql`);
+
+global.baza = mysql.createConnection({
+    host: ``,
+    user: ``,
+    password: ``,
+    database: ``
+})
+
+baza.connect(function(err ) {
+    if (err) {
+        console.log(err)
+    }
+})
+
+client.on(`ready`, async () => {
+    onReady = require(`./events/ready.js`)
+    onReady(client, console)
+})
+
+
+
+client.on(`message`, async message => {
+
+
+
+//---------------[REAKCJA NA PING]-----------------\\
+if (message.content === `<@!${client.user.id}>` || message.content === `<@${client.user.id}>`) {
+    message.react(`🏓`)
+    const prefix = db.fetch(`${message.guild.id}.prefix`) || gprefix
+    let totalSeconds = (client.uptime / 1000)
+    let days = Math.floor(totalSeconds / 86400)
+    totalSeconds %= 86400
+    let hours = Math.floor(totalSeconds / 3600)
+    totalSeconds %= 3600
+    let minutes = Math.floor(totalSeconds / 60)
+    let seconds = Math.floor(totalSeconds % 60)
+    let uptime = (days > 0 ? days + `d, ` : ``) + (hours > 0 ? hours + `h, ` : ``) + (minutes > 0 ? minutes + `m, ` : ``) + (seconds > 0 ? seconds + `s` : ``)
+    let botuptime = uptime || `0s`
+    const embed = new Discord.MessageEmbed()
+    embed
+        .setColor(kolor_embeda)
+        .setDescription(`> **Hejka! Jestem ${client.user.username}, wielofunkcyjnym botem do discorda!**\n> **Listę komend znajdziesz po wpisaniu __${prefix}pomoc__!**`)
+        .addField(`${emotki.zebatki}・Prefix:`, prefix)
+        .addField(`🏓・Ping:`, `${client.ws.ping}ms`)
+        .addField(`${emotki.support}・Uptime:`, botuptime)
+        .setTimestamp()
+        .setFooter(`${message.author.tag} (${message.author.id})`, message.author.displayAvatarURL({dynamic: true}))
+    message.channel.send(embed)
+}
+
+global.permisjalicencji = message.guild.roles.cache.get(uprlicencja)
+//---------------[REAKCJA NA PING]-----------------\\
+
+//-----------------------------[AntyZaproszenia]-----------------------------\\
+        let link = ["discord.gg", "discord.com/invite", "dsc.gg/"]
+        if (link.some(word => message.content.toLowerCase().includes(word))) {
+            message.delete()
+            const embed3 = new Discord.MessageEmbed()
+        
+            .setDescription(`${emotki.nie} Na tym serwerze nie możesz wysyłać zaproszeń do innych discordów!`)
+            .setColor(`RED`)
+            .setFooter(`${message.author.tag} (${message.author.id})`, message.author.displayAvatarURL({ dynamic: true }))
+            return message.channel.send(embed3)
+        }
+    
+//-----------------------------[AntyZaproszenia]-----------------------------\\
+
+
+    
+})
+
+
+client.login(token).then(() => {
+}).catch(() => {
+        console.log(chalk.red(`============================`))
+    setInterval(async function () {
+        console.log(chalk.red(`Fejkowy wlascicielu daj rel tokenik!`))
+        console.log(chalk.red(`============================`))
+    }, 2500)
+})
